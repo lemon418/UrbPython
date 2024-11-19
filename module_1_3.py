8decd62efc7d4de680ce556e8236638312308fb5
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-11-19 18:58:48 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,23 @@
+name = "Dmitry"
+print('Name:', name)
+print('Name: ' + name)
+
+
+
+age = 38
+print('Age:', age)
+print('Age: ' + str(age))
+
+
+
+
+age = age + 2
+print('New Age:', age)
+print('New Age: ' + str(age))
+
+
+
+is_student = True
+print('Is Student:', is_student)
+print('Is Student: ' + str(is_student))
+
