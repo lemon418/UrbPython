6d685a2210c02e7a68fd28af4c1948d71707bf91
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-11-20 22:12:09 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+task_done = 12
+sum_time = 1.5
+course_name = "Python"
+task_time = 1.5 / 12
+
+print('Курс: ' + course_name +  ', всего задач: ' + str(task_done) + ', затрачено часов: ' + str(sum_time) + ', среднее время выполнения ' + str(task_time) + ' часа.')
