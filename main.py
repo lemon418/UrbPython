541103f21919f1d27d5343461b384afb8b6d9a14
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-11-27 22:20:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-# "1st program"
-print(9  ** 0.5 * 5)
-
-# "2nd program"
-print(99.9 > 99.8 and 1000 != 1000.1)
-
-# "3rd program"
-print (2 * 2 + 2)
-print (2 * (2 + 2))
-print (  (2 * 2 + 2) ==  ( 2 * (2 + 2) )  )
-
-# "4th program"
-
-print(float('123.456'))
-print(float('123.456') * 10)
-print(int(float('123.456') * 10) % 10)
-
-# Немного не понял задачу. Возможно, что не все условия задачи выполнены.
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
