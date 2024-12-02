69ff22173206f2c68ccb220503a12da9e98bc803
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-12-02 20:41:28 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,26 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = numbers
+not_primes = []
+
+for i in range(len(numbers)):
+    # is_prime = True
+    num = numbers[i]
+    if num == 1:
+        continue
+
+    for j in range(2, num):
+        if num % j == 0:
+            # is_prime = False
+            not_primes.append(num)
+            break
+
+
+for i in range(len(numbers) + 1 ):
+    for j in not_primes:
+        if i == j:
+            primes.remove(i)
+primes.remove(1)
+
+
+print('Primes: ', primes)
+print('Not Primes: ', not_primes)
