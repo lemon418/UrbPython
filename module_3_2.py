3752bca7c93f99f8c78391c5191cbac4651e50d3
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-12-07 23:37:19 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -20,9 +20,3 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
-
-
-
-
-
