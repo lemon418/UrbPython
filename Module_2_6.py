665d9857a82ef198406604468ec679646664314f
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-12-07 17:51:12 +0300

    Add files via upload

---
+++ Module_2_6.py
@@ -0,0 +1,18 @@
+import random
+
+num = random.randint(3, 21)
+print(num)
+
+num_list1 = list(range(1, num))
+num_list2 = list(range(1, num))
+password =  []
+
+for i in num_list1:
+    for j in num_list2:
+        if num % (i + j) == 0:
+            password.append(i)
+            password.append(j)
+    num_list2.remove(i)
+    # print("List2:", num_list2)
+
+print("Наш пароль: ", *password)
