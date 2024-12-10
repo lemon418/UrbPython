146b17456d00f5a5ce2d9f3dfb75df1b15e6f948
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-12-10 18:53:34 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,24 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_w = root_word.lower()
+
+    for word in other_words:
+        other_w = word.lower()
+        txt = other_w.find(root_word)
+        if txt > -1:
+            same_words.append(word)
+
+    for word in other_words:
+        other_w = word.lower()
+        txt = root_w.find(other_w)
+        if txt > -1:
+            same_words.append(word)
+    print(same_words)
+
+    return same_words
+
+
+
+same = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
