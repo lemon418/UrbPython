c5af2e3d92cf36458c39c5aed4d4da820f37e4ef
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2024-12-29 10:56:48 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,18 @@
+class House():
+    def __init__(self, name, numbers_of_floor):
+        self.name = name
+        self.numbers_of_floor = numbers_of_floor
+
+    def go_to(self, new_floor):
+        if (self.numbers_of_floor < new_floor) or (new_floor < 1):
+            print('Такого этажа не существует')
+
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+yalta = House('ЖК "Yalta"', 20)
+yalta.go_to(21)
+
+hotel = House('Hotel', 18)
+yalta.go_to(5)
