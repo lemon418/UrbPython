55df350af3cc3b065cd7a0fbbd6b548618af7a5d
PerfectDecay <55945297+lemon418@users.noreply.github.com>
2025-01-11 16:13:48 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,62 @@
+import random
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = int(speed)
+    def move(self, dx, dy, dz):
+        int(dx)
+        if dz < 0:
+            print("It's too deep, I can't dive :(")
+        else:
+            self._cords = [dx*self.speed, dy*self.speed, dz*self.speed]
+
+    def get_cords(self):
+        print( f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {int(self._cords[2])}")
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("I'm peacfull ;)")
+        elif self._DEGREE_OF_DANGER >= 5:
+            print("Be carefull, I'm attacking you!")
+    def speak(self):
+        print(self.sound)
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+class Bird(Animal):
+    beak = True
+    def __init__(self, speed):
+        super().__init__(speed)
+
+    def lay_eggs(self):
+        eggs_ = random.randint (0,4)
+        print(f"Here are(is) {eggs_} eggs for you")
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+    def dive_in(self, dz):
+         dive = abs(dz)*self.speed/2
+         self._cords[2] = self._cords[2] - dive
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    sound = "Click-click-click"
+    def __init__(self, speed):
+        super().__init__(speed)
+
+db = Duckbill(10)
+print(db.live)
+print(db.beak)
+db.speak()
+db.attack()
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+db.lay_eggs()
+db.move(1, 4,  -1)
+print(db._cords)
